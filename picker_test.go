9ffddd13fc3739b332a0d4bf0562c949304bde78
Igor <igor.barbosa1920@gmail.com>
2020-02-03 12:54:16 -0300

    Testing many times RollDice

--- picker_test.go
+++ picker_test.go
@@ -2,7 +2,6 @@ package picker
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestCreateInterval(t *testing.T) {
@@ -32,25 +31,28 @@ func TestRollDice(t *testing.T) {
 	c.Add("b", 20)
 	c.Add("c", 30)
 	c.Add("d", 40)
-	itvl, err := c.RollDice()
-	fmt.Println(itvl)
-	if err != nil {
-		t.Error("Something goes wrong" + err.Error())
-	}
-	if itvl == "" {
-		t.Error("Something goes wrong" + err.Error())
+	for i := 0; i < 500; i++ {
+		itvl, err := c.RollDice()
+		if err != nil {
+			t.Error("Something goes wrong" + err.Error())
+		}
+		if itvl == "" {
+			t.Error("Something goes wrong" + err.Error())
+		}
 	}
 }
 
 func TestFree(t *testing.T) {
 	c := New()
-	c.Add("a", 10)
-	c.Add("b", 20)
-	c.Add("c", 30)
-	c.Add("d", 40)
-	c.Reset()
-	_, err := c.RollDice()
-	if err == nil {
-		t.Error("Picker should return an error" + err.Error())
+	for i := 0; i < 500; i++ {
+		c.Add("a", 10)
+		c.Add("b", 20)
+		c.Add("c", 30)
+		c.Add("d", 40)
+		c.Reset()
+		_, err := c.RollDice()
+		if err == nil {
+			t.Error("Picker should return an error" + err.Error())
+		}
 	}
 }
