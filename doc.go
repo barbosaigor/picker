5c832f47fbbcc29f6d34cd37c437a4261b3fdc4b
Igor <igor.barbosa1920@gmail.com>
2020-05-02 16:28:41 -0300

    moved package description to doc.go

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package picker implements an algorithm that
+// randomly selects an element, taking into account each element probability.
+// Higher weight elements are more likely to be choosen.
+package picker
